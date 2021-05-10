4063701fb150544be6eb38f43b213855c1e31a76
wguimaraes <willian.guimaraes@qualitor.com.br>
2021-05-10 19:48:29 -0300

    Adicionando suporte a migrations (flyway)

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,1 @@
+INSERT INTO `person` VALUES (1,'Protasio Alves 8301','William','M','Guimarães'),(2,'Protasio Alves 8301','Anahi111111','F','Guimarães'),(4,'Protasio Alves 8301','Anahi111','F','Guimarães');
