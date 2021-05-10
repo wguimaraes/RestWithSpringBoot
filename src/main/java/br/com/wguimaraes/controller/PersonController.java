428f14fb09641718ce201ef92b0736cb8d9231e7
wguimaraes <willian.guimaraes@qualitor.com.br>
2021-05-10 19:07:58 -0300

    Versionando endpoins e criando custom converter

--- src/main/java/br/com/wguimaraes/controller/PersonController.java
+++ src/main/java/br/com/wguimaraes/controller/PersonController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.wguimaraes.data.vo.PersonVO;
+import br.com.wguimaraes.data.vo.v2.PersonVOV2;
 import br.com.wguimaraes.services.PersonService;
 
 @RestController
@@ -27,6 +28,11 @@ public class PersonController {
 	@PostMapping
 	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
+	}
+	
+	@PostMapping("/v2")
+	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
+		return service.createV2(person);
 	}
 	
 	@PutMapping
--- src/main/java/br/com/wguimaraes/services/PersonService.java
+++ src/main/java/br/com/wguimaraes/services/PersonService.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.wguimaraes.converter.DozerConverter;
+import br.com.wguimaraes.converter.custom.PersonConverter;
 import br.com.wguimaraes.data.model.Person;
 import br.com.wguimaraes.data.vo.PersonVO;
+import br.com.wguimaraes.data.vo.v2.PersonVOV2;
 import br.com.wguimaraes.exception.ResourceNotFoundException;
 import br.com.wguimaraes.repository.PersonRepository;
 
@@ -17,9 +19,18 @@ public class PersonService {
 	@Autowired
 	PersonRepository repository;
 	
+	@Autowired
+	PersonConverter converter;
+	
 	public PersonVO create(PersonVO person) {
 		Person entity = DozerConverter.parseObject(person, Person.class);
 		PersonVO vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+	
+	public PersonVOV2 createV2(PersonVOV2 person) {
+		Person entity = converter.convertVOToEntity(person);
+		PersonVOV2 vo = converter.convertEntityToVO(repository.save(entity));
 		return vo;
 	}
 	
