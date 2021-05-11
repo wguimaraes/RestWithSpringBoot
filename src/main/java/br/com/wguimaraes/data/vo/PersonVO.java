ee4f46f8dedb41afdecb37a679728fd6eed36e46
wguimaraes <willian.guimaraes@qualitor.com.br>
2021-05-10 21:31:27 -0300

    Implementado JSON serialization

--- src/main/java/br/com/wguimaraes/data/vo/PersonVO.java
+++ src/main/java/br/com/wguimaraes/data/vo/PersonVO.java
@@ -2,15 +2,23 @@ package br.com.wguimaraes.data.vo;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 
+	@JsonProperty("last_name")
 	private String firstName;
+	@JsonProperty("first_name")
 	private String lastName;
 	private String address;
+	@JsonIgnore
 	private String gender;
 	
 	public PersonVO() {
@@ -76,7 +84,7 @@ public class PersonVO implements Serializable {
 	public String getFirstName() {
 		return this.firstName;
 	}
-
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
